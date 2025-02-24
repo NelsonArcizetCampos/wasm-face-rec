3209a1e9741a389d8bee5b454f11ee5bad8eb5c9
Nelson Campos <80530709+NelsonArcizetCampos@users.noreply.github.com>
2025-02-24 15:42:56 +0000

    Deploy updated WebAssembly-based face recognition app (#3)
    * Finalized WebAssembly setup, placeholder face detection in place, awaiting new implementation (#1)
    
    * Force track pkg directory

---
+++ pkg/wasm_face_rec_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_faceboundingbox_free: (a: number, b: number) => void;
+export const __wbg_get_faceboundingbox_x: (a: number) => number;
+export const __wbg_set_faceboundingbox_x: (a: number, b: number) => void;
+export const __wbg_get_faceboundingbox_y: (a: number) => number;
+export const __wbg_set_faceboundingbox_y: (a: number, b: number) => void;
+export const __wbg_get_faceboundingbox_width: (a: number) => number;
+export const __wbg_set_faceboundingbox_width: (a: number, b: number) => void;
+export const __wbg_get_faceboundingbox_height: (a: number) => number;
+export const __wbg_set_faceboundingbox_height: (a: number, b: number) => void;
+export const detect_faces: (a: number, b: number, c: number, d: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
